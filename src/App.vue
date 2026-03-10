a8ea08091d0adb0addd1ba072cdf5811b06e6c06
HU TAO <107668202+HuTao1213@users.noreply.github.com>
2026-03-10 17:49:02 +0800

    Update app.vue to add footer with beian number

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <footer>
+      <div style="text-align: center; font-size: small;">
+        湘ICP备2026006740号
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+/* Add your styles here */
+</style>
